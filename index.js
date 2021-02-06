1cd8e57e86507114f24151fc819991ffad67b486
nielvid <>
2021-02-06 04:29:51 +0100

    cors error fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 app.use(express.json())
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://dgtal-ecommerce.herokuapp.com/"
 };
 app.use(cors(corsOptions));
 
